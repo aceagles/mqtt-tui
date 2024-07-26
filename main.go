a10d5560ec6926efc57f1f6b96966669d74ff36b
Adam Eagles <ad.c.eagles@gmail.com>
2024-07-26 20:55:52 +0100

    MQTT connection within the state

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
+	"sort"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func listenForActivity(sub chan mqttMessage) tea.Cmd {
+func ConnectToMQTT(m model) tea.Cmd {
 	return func() tea.Msg {
-		for {
-			time.Sleep(time.Millisecond * time.Duration(rand.Int63n(900)+100)) // nolint:gosec
-			sub <- mqttMessage{}
+		clientOps := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", m.clientUri)).SetClientID("mqtt-tui")
+		client := mqtt.NewClient(clientOps)
+		isConnected := false
+		for !isConnected {
+			token := client.Connect()
+			if token.Error() != nil {
+				return token.Error()
+			}
+			if client.IsConnected() {
+				isConnected = true
+			} else {
+				time.Sleep(1 * time.Second)
+			}
 		}
+		for _, topic := range m.subscribedTopics {
+			client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+				m.sub <- mqttMessage{topic: msg.Topic(), payload: string(msg.Payload())}
+			})
+		}
+		return client
 	}
 }
 
@@ -32,16 +48,19 @@ func waitForActivity(sub chan mqttMessage) tea.Cmd {
 }
 
 type model struct {
-	sub          chan mqttMessage // where we'll receive activity notifications
-	client       mqtt.Client
-	messageOrder []string
-	messages     map[string]string
-	quitting     bool
+	sub              chan mqttMessage // where we'll receive activity notifications
+	client           mqtt.Client
+	clientUri        string
+	subscribedTopics []string
+	messageOrder     []string
+	messages         map[string]string
+	quitting         bool
 }
 
 func (m model) Init() tea.Cmd {
 	return tea.Batch(
 		waitForActivity(m.sub), // wait for activity
+		ConnectToMQTT(m),       // connect to MQTT
 	)
 }
 
@@ -53,15 +72,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case mqttMessage:
 		if _, ok := m.messages[val.topic]; !ok {
 			m.messageOrder = append(m.messageOrder, val.topic)
+			sort.Strings(m.messageOrder)
 		}
 		m.messages[val.topic] = val.payload
 		return m, waitForActivity(m.sub) // wait for next event
+	case mqtt.Client:
+		m.client = val
+		return m, nil
+	case error:
+		fmt.Printf("error: %v", val)
+		return m, tea.Quit
 	default:
 		return m, nil
 	}
 }
 
 func (m model) View() string {
+	if m.client == nil || !m.client.IsConnected() {
+		return "Connecting to MQTT..."
+	}
 	var s string
 	for _, topic := range m.messageOrder {
 		s += fmt.Sprintf("%s: %s\n", topic, m.messages[topic])
@@ -73,28 +102,24 @@ func (m model) View() string {
 }
 
 func main() {
-
-	broker := "tcp://localhost:1883"
-	clientOps := mqtt.NewClientOptions().AddBroker(broker).SetClientID("mqtt-tui")
-	client := mqtt.NewClient(clientOps)
-	token := client.Connect()
-	if token.Error() != nil {
-		fmt.Println("could not connect to MQTT broker")
+	broker := os.Args[1]
+	if broker == "" {
+		fmt.Println("please provide a broker address")
 		os.Exit(1)
 	}
-	defer client.Disconnect(250)
+	topics := os.Args[2:]
+	if len(topics) == 0 {
+		fmt.Println("please provide at least one topic")
+		os.Exit(1)
+	}
+
 	subChan := make(chan mqttMessage)
 	p := tea.NewProgram(model{
-		sub:          subChan,
-		client:       client,
-		messages:     make(map[string]string),
-		messageOrder: make([]string, 0),
-	})
-	<-token.Done()
-
-	client.Subscribe("#", 0, func(client mqtt.Client, msg mqtt.Message) {
-
-		subChan <- mqttMessage{topic: msg.Topic(), payload: string(msg.Payload())}
+		sub:              subChan,
+		clientUri:        broker,
+		subscribedTopics: topics,
+		messages:         make(map[string]string),
+		messageOrder:     make([]string, 0),
 	})
 
 	if _, err := p.Run(); err != nil {
